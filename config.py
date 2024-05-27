10b0ca4130e53bc2ceb0387349fc6199f57782fa
dsantra80 <60920557+dsantra80@users.noreply.github.com>
2024-05-27 16:14:51 +0530

    Updated config.py

--- config.py
+++ config.py
@@ -8,4 +8,6 @@ class Config:
     HF_TOKEN = os.getenv("HF_TOKEN")
     MODEL_NAME = os.getenv("MODEL_NAME")
     MAX_TOKENS = int(os.getenv("MAX_TOKENS", 50))  # Default value if not set
-    TEMPERATURE = float(os.getenv("TEMPERATURE", 0.7))  # Default value if not set
+    TEMPERATURE = float(os.getenv(
+        "TEMPERATURE", 0.7
+    ))  # Default value if not set
